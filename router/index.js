380b2f75c777ab9b67d9b1e33206c31e58e3a5a9
Seu Nome <MendoncaGabriel1997@gmail.com>
2023-12-28 15:51:31 -0300

    add

--- router/index.js
+++ router/index.js
@@ -4,6 +4,41 @@ const Game = require('../model/Game.js')
 const cache = require('memory-cache');
 
 
+router.get('/page/:pg', async (req, res) => {
+    const pg = req.params.pg
+    const pageSize = 20; 
+    const cacheKey = req.originalUrl || req.url;
+
+    // Verificar se os dados estão em cache
+    const cachedData = cache.get(cacheKey);
+
+    if (cachedData) {
+        // Se estiver em cache, renderizar a partir do cache
+        console.log('----------------------------');
+
+        console.log('Página principal cacheada!');
+        console.log('----------------------------');
+        res.render('home', { title: 'Home', data: cachedData, page: pg });
+    } else {
+        try {
+            // Se não estiver em cache, buscar dados no banco de dados
+            const data = await Game.find().skip(pg * pageSize).limit(pageSize).exec();
+            console.log('-------------------------------------');
+            console.log('Cacheando página principal!');
+            console.log('-------------------------------------');
+
+            // Colocar os dados em cache por 24 horas
+            cache.put(cacheKey, data, 24 * 60 * 60 * 1000);
+
+            // Renderizar a página com os dados recém-obtidos
+            res.render('home', { title: 'Home', data: data, page: pg });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Erro interno do servidor');
+        }
+    }
+});
+
 router.get('/', async (req, res) => {
     const pg = 0; // Página atual (se você quiser implementar paginação)
     const pageSize = 20; // Número de documentos a serem recuperados
@@ -17,7 +52,7 @@ router.get('/', async (req, res) => {
         console.log('----------------------------');
         console.log('Página principal cacheada!');
         console.log('----------------------------');
-        res.render('home', { title: 'Home', data: cachedData });
+        res.render('home', { title: 'Home', data: cachedData, page: pg });
     } else {
         try {
             // Se não estiver em cache, buscar dados no banco de dados
@@ -30,7 +65,7 @@ router.get('/', async (req, res) => {
             cache.put(cacheKey, data, 24 * 60 * 60 * 1000);
 
             // Renderizar a página com os dados recém-obtidos
-            res.render('home', { title: 'Home', data: data });
+            res.render('home', { title: 'Home', data: data, page: pg });
         } catch (error) {
             console.error(error);
             res.status(500).send('Erro interno do servidor');
