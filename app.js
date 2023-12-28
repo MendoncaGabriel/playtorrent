97fe03666e3506e1f93755b0ad487913aaea0619
Seu Nome <MendoncaGabriel1997@gmail.com>
2023-12-28 14:11:36 -0300

    add

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-
-
 require("dotenv").config(); 
 require('./connect/MongoDB.js')
 
@@ -12,7 +10,7 @@ app.set('view engine', 'ejs')
 const routes = require(__dirname + '/router/index.js')
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
-// app.set('views', path.join(process.cwd(), 'views'));
+
 console.log('Caminho das visualizações:', path.join(process.cwd(), 'views'));
 
 // Configurar Content Security Policy (CSP)
@@ -21,12 +19,9 @@ app.use((req, res, next) => {
     next();
 });
 
-
 app.use((req, res) => {
     res.status(404).render('404')
 });
-
-
 
 app.listen(3000, ()=>{
     console.log('http://localhost:3000')
