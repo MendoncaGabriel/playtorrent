52dce8ff8abf0b58d8935bb102b60b030ab41623
Seu Nome <MendoncaGabriel1997@gmail.com>
2023-12-30 13:13:53 -0300

    add

--- router/index.js
+++ router/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Game = require('../model/Game.js');
 const cache = require('memory-cache');
-const cacheTime = 2 * 60 * 60 * 1000
+const cacheTime = 24 * 60 * 60 * 1000
 
 router.get('/page/:pg', async (req, res) => {
     const pg = req.params.pg
@@ -25,6 +25,7 @@ router.get('/page/:pg', async (req, res) => {
 })
 
 
+//modelo de teste
 router.get('/', async (req, res) => {
     const pg = 0;
     const pageSize = 20;
@@ -32,30 +33,45 @@ router.get('/', async (req, res) => {
     const cachedData = cache.get(cacheKey);
 
     try {
+        if (cachedData) {
+            const { data, dataTopViews, dataTopDownloads } = cachedData;
+
+            if (!data || !dataTopViews || !dataTopDownloads) {
+                console.error('Dados ausentes ou inválidos no cache.');
+                return res.status(500).send('Erro interno do servidor');
+            }
+
+            console.log('Página com cache!');
+            return res.render('home', { title: 'Home', data, page: pg, dataTopViews, dataTopDownloads });
+        }
+
         const [data, dataTopViews, dataTopDownloads] = await Promise.all([
             getGamesWithPagination(pg, pageSize),
             getTopGames('views', 10),
             getTopGames('download', 10)
         ]);
 
-
-        if (cachedData) {
-            res.render('home', { title: 'Home', data: cachedData, page: pg, dataTopViews, dataTopDownloads });
-            console.log('Pagina com cache!')
-        } else {
-            cache.put(cacheKey, data, cacheTime);
-            console.log('Sem cache, consultando banco de dados e cacheando...')
-            res.render('home', { title: 'Home', data, page: pg, dataTopViews, dataTopDownloads });
+        if (!data || !dataTopViews || !dataTopDownloads) {
+            console.error('Dados ausentes ou inválidos.');
+            return res.status(500).send('Erro interno do servidor');
         }
+
+        cache.put(cacheKey, { data, dataTopViews, dataTopDownloads }, cacheTime);
+        console.log('Sem cache, consultando banco de dados e cacheando...');
+
+        res.render('home', { title: 'Home', data, page: pg, dataTopViews, dataTopDownloads });
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Erro ao buscar dados:', error);
         res.status(500).send('Erro interno do servidor');
     }
 });
 
+
+
 async function getGamesWithPagination(page, size) {
     return Game.find().skip(page * size).limit(size).exec();
 }
+
 async function getTopGames(field, limit) {
     const matchQuery = { [field]: { $exists: true } };
     const sortQuery = { [field]: -1 };
@@ -66,6 +82,57 @@ async function getTopGames(field, limit) {
         { $limit: limit }
     ]);
 }
+
+
+
+
+
+
+
+
+
+
+//modelo estavel
+// router.get('/', async (req, res) => {
+//     const pg = 0;
+//     const pageSize = 20;
+//     const cacheKey = req.originalUrl || req.url;
+//     const cachedData = cache.get(cacheKey);
+
+//     try {
+//         const [data, dataTopViews, dataTopDownloads] = await Promise.all([
+//             getGamesWithPagination(pg, pageSize),
+//             getTopGames('views', 10),
+//             getTopGames('download', 10)
+//         ]);
+
+
+//         if (cachedData) {
+//             res.render('home', { title: 'Home', data: cachedData, page: pg, dataTopViews, dataTopDownloads });
+//             console.log('Pagina com cache!')
+//         } else {
+//             cache.put(cacheKey, data, cacheTime);
+//             console.log('Sem cache, consultando banco de dados e cacheando...')
+//             res.render('home', { title: 'Home', data, page: pg, dataTopViews, dataTopDownloads });
+//         }
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//         res.status(500).send('Erro interno do servidor');
+//     }
+// });
+// async function getGamesWithPagination(page, size) {
+//     return Game.find().skip(page * size).limit(size).exec();
+// }
+// async function getTopGames(field, limit) {
+//     const matchQuery = { [field]: { $exists: true } };
+//     const sortQuery = { [field]: -1 };
+
+//     return Game.aggregate([
+//         { $match: matchQuery },
+//         { $sort: sortQuery },
+//         { $limit: limit }
+//     ]);
+// }
 
 
 router.get('/download/:name', async (req, res) => {
