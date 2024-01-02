fbe312873ac3470e6b7e026092d8dfd550a16a31
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-01-01 22:21:36 -0300

    add

--- router/index.js
+++ router/index.js
@@ -2,30 +2,80 @@ const express = require('express');
 const router = express.Router();
 const Game = require('../model/Game.js');
 const cache = require('memory-cache');
+const http = require('http');
+const https = require('https');
 const cacheTime = 24 * 60 * 60 * 1000
 
-router.get('/page/:pg', async (req, res) => {
-    const pg = req.params.pg
-    const pageSize = 20; 
-    const cacheKey = req.originalUrl || req.url;
-    const cachedData = cache.get(cacheKey);
-
-    if (cachedData) {
-        res.render('page', { title: 'Home', data: cachedData, page: pg });
-    } else {
-        try {
-            const data = await Game.find().skip(pg * pageSize).limit(pageSize).exec();
-            cache.put(cacheKey, data, cacheTime);
-            res.render('page', { title: 'Home', data: data, page: pg });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Erro interno do servidor');
+//Funções--------------------------------------------------------
+async function isImageValid(teste) {
+    let url = 'http://localhost:3000/cover/' + teste
+    try {
+        // Verifica se a URL termina com uma extensão de imagem comum
+        const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
+        const isImageExtension = imageExtensions.some(ext => url.toLowerCase().endsWith(ext));
+    
+        if (!isImageExtension) {
+        return false;
         }
+    
+        // Determina o módulo HTTP com base no protocolo da URL
+        const protocol = url.startsWith('https') ? https : http;
+    
+        // Faz uma requisição HEAD para obter o cabeçalho da imagem sem baixar todo o conteúdo
+        const response = await new Promise((resolve, reject) => {
+        const req = protocol.request(url, { method: 'HEAD' }, resolve);
+        req.on('error', reject);
+        req.end();
+        });
+    
+        // Verifica se o cabeçalho Content-Type indica que é uma imagem
+        const contentType = response.headers['content-type'];
+        if (!contentType || !contentType.startsWith('image/')) {
+        return false;
+        }
+    
+        // Pode adicionar mais verificações aqui, se necessário
+    
+        return true;
+    } catch (error) {
+        console.error('Erro ao verificar a imagem:', error.message);
+        return false;
     }
-})
+}
+async function getGamesWithPagination(page, size) {
+    return Game.find().skip(page * size).limit(size).exec();
+}
+async function getTopGames(field, limit) {
+    const matchQuery = { [field]: { $exists: true } };
+    const sortQuery = { [field]: -1 };
 
+    return Game.aggregate([
+        { $match: matchQuery },
+        { $sort: sortQuery },
+        { $limit: limit }
+    ]);
+}
+async function contViwer(id){
+    try {
+        const game = await Game.findById(id);
 
-//modelo de teste
+        if (!game) {
+            return res.status(404).json({ msg: 'Jogo não encontrado' });
+        }
+
+        // Se `views` não existe ou é `undefined` ou `null`
+        if (!game.views) {
+            await Game.findByIdAndUpdate(id, { views: 1 }, { new: true });
+        }
+
+        // Se `views` existe, incrementa +1
+        await Game.findByIdAndUpdate(id, { $inc: { views: 1 } }, { new: true });
+    } catch (err) {
+        console.error('Erro ao contar visualização:', err);
+    }
+}
+
+//Rotas--------------------------------------------------------
 router.get('/', async (req, res) => {
     const pg = 0;
     const pageSize = 20;
@@ -64,100 +114,74 @@ router.get('/', async (req, res) => {
         console.error('Erro ao buscar dados:', error);
         res.status(500).send('Erro interno do servidor');
     }
-});
+})
 
+router.get('/page/:pg', async (req, res) => {
+    const pg = req.params.pg
+    const pageSize = 20; 
+    const cacheKey = req.originalUrl || req.url;
+    const cachedData = cache.get(cacheKey);
 
+    if (cachedData) {
+       
+        console.log(cachedData)
+        console.log('CACHE')
+        res.render('page', { title: 'Home', data: cachedData, page: pg });
+    } else {
+        try {
+            const data = await Game.find().skip(pg * pageSize).limit(pageSize).exec();
+            cache.put(cacheKey, data, cacheTime);
+            console.log('NO_CACHE')
+            res.render('page', { title: 'Home', data: data, page: pg });
+          
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Erro interno do servidor');
+        }
+    }
+})
 
-async function getGamesWithPagination(page, size) {
-    return Game.find().skip(page * size).limit(size).exec();
-}
-
-async function getTopGames(field, limit) {
-    const matchQuery = { [field]: { $exists: true } };
-    const sortQuery = { [field]: -1 };
-
-    return Game.aggregate([
-        { $match: matchQuery },
-        { $sort: sortQuery },
-        { $limit: limit }
-    ]);
-}
-
-
-
-
-
-
-
-
-
-
-//modelo estavel
-// router.get('/', async (req, res) => {
-//     const pg = 0;
-//     const pageSize = 20;
-//     const cacheKey = req.originalUrl || req.url;
-//     const cachedData = cache.get(cacheKey);
-
-//     try {
-//         const [data, dataTopViews, dataTopDownloads] = await Promise.all([
-//             getGamesWithPagination(pg, pageSize),
-//             getTopGames('views', 10),
-//             getTopGames('download', 10)
-//         ]);
-
-
-//         if (cachedData) {
-//             res.render('home', { title: 'Home', data: cachedData, page: pg, dataTopViews, dataTopDownloads });
-//             console.log('Pagina com cache!')
-//         } else {
-//             cache.put(cacheKey, data, cacheTime);
-//             console.log('Sem cache, consultando banco de dados e cacheando...')
-//             res.render('home', { title: 'Home', data, page: pg, dataTopViews, dataTopDownloads });
-//         }
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         res.status(500).send('Erro interno do servidor');
-//     }
-// });
-// async function getGamesWithPagination(page, size) {
-//     return Game.find().skip(page * size).limit(size).exec();
-// }
-// async function getTopGames(field, limit) {
-//     const matchQuery = { [field]: { $exists: true } };
-//     const sortQuery = { [field]: -1 };
-
-//     return Game.aggregate([
-//         { $match: matchQuery },
-//         { $sort: sortQuery },
-//         { $limit: limit }
-//     ]);
-// }
-
+router.get('/edit-page', (req, res)=>{
+    
+    res.render('editPage')
+})
 
 router.get('/download/:name', async (req, res) => {
     try {
         const nameTratado = req.params.name.replace(/-/g, ' ');
+        const cachedData = cache.get(nameTratado);
+ 
         if (!nameTratado) {
             return res.status(422).json({ msg: 'Não encontrado!' });
         }
-        const cachedData = cache.get(nameTratado);
+        
         if (cachedData) {
-            return res.render('game', { data: cachedData });
+            console.log('CACHE')
+            res.render('game', { data: cachedData });
+            contViwer(`${cachedData._id}`)
+        }else{
+            console.log('NO-CACHE')
+
+
+            const data = await Game.findOne({ name: { $regex: new RegExp(`^${nameTratado}$`, 'i') } })
+
+
+
+            if (data) {
+                cache.put(nameTratado, data, cacheTime); 
+                res.render('game', { data: data });
+                contViwer(`${data._id}`) 
+            } else {
+                res.status(404).json({ msg: 'Não encontrado!' });
+            }
         }
-        const data = await Game.findOne({ name: { $regex: new RegExp(`^${nameTratado}$`, 'i') } });
-        if (data) {
-            cache.put(nameTratado, data, cacheTime); 
-            res.render('game', { data: data });
-        } else {
-            res.status(404).json({ msg: 'Não encontrado!' });
-        }
+
+
     } catch (err) {
         console.error(err);
         res.status(500).send('Erro ao carregar a página!');
     }
 })
-
 
 router.patch('/downloadCont/:id', async (req, res) => {
     try {
@@ -185,33 +209,7 @@ router.patch('/downloadCont/:id', async (req, res) => {
         console.error('Erro ao contar download:', err);
         res.status(500).send('Erro ao contar download');
     }
-});
-
-
-router.patch('/viewCont/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const game = await Game.findById(id);
-
-        if (!game) {
-            return res.status(404).json({ msg: 'Jogo não encontrado' });
-        }
-
-        // Se `views` não existe ou é `undefined` ou `null`
-        if (!game.views) {
-            const newUpdate = await Game.findByIdAndUpdate(id, { views: 1 }, { new: true });
-            return res.status(200).json({ msg: 'Primeira visualização contada com sucesso', views: newUpdate.views });
-        }
-
-        // Se `views` existe, incrementa
-        const newUpdate = await Game.findByIdAndUpdate(id, { $inc: { views: 1 } }, { new: true });
-        return res.status(200).json({ msg: 'Visualização contada com sucesso', views: newUpdate.views });
-    } catch (err) {
-        console.error('Erro ao contar visualização:', err);
-        res.status(500).send('Erro ao contar visualização');
-    }
-});
-
+})
 
 router.get('/analytics', async (req, res) => {
     try {
@@ -224,46 +222,7 @@ router.get('/analytics', async (req, res) => {
         console.error('Erro ao obter jogos com visualizações:', err);
         res.status(500).send('Erro ao obter jogos com visualizações');
     }
-});
-
-async function isImageValid(teste) {
-    let url = 'http://localhost:3000/cover/' + teste
-    try {
-        // Verifica se a URL termina com uma extensão de imagem comum
-        const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
-        const isImageExtension = imageExtensions.some(ext => url.toLowerCase().endsWith(ext));
-    
-        if (!isImageExtension) {
-        return false;
-        }
-    
-        // Determina o módulo HTTP com base no protocolo da URL
-        const protocol = url.startsWith('https') ? https : http;
-    
-        // Faz uma requisição HEAD para obter o cabeçalho da imagem sem baixar todo o conteúdo
-        const response = await new Promise((resolve, reject) => {
-        const req = protocol.request(url, { method: 'HEAD' }, resolve);
-        req.on('error', reject);
-        req.end();
-        });
-    
-        // Verifica se o cabeçalho Content-Type indica que é uma imagem
-        const contentType = response.headers['content-type'];
-        if (!contentType || !contentType.startsWith('image/')) {
-        return false;
-        }
-    
-        // Pode adicionar mais verificações aqui, se necessário
-    
-        return true;
-    } catch (error) {
-        console.error('Erro ao verificar a imagem:', error.message);
-        return false;
-    }
-}
-const http = require('http');
-const https = require('https');
-
+})
 
 router.get('/checkImage', async (req, res) => {
     try {
@@ -282,8 +241,7 @@ router.get('/checkImage', async (req, res) => {
         console.error('Erro no teste de imagem:', error);
         res.status(500).send('Erro no teste de imagem');
     }
-});
-
+})
 
 router.get('/renameImage', async (req, res) => {
     try {
@@ -298,8 +256,7 @@ router.get('/renameImage', async (req, res) => {
         console.error('Error renaming images:', error);
         res.status(500).send('Internal Server Error');
     }
-});
-
+})
 
 router.get('/search/:name', async (req, res) => {
     try {
