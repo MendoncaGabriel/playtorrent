9484ca1adbb1d5e4cb8da85e8289cea21281251d
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-01-04 21:49:23 -0300

    add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 require("dotenv").config(); 
 require('./connect/MongoDB.js');
+const Game = require('./model/gameSchema.js');
 
 //Portas e Caminhos
 const PORT = 3000;
@@ -23,6 +24,63 @@ const routesServices = require('./router/services.js');
 
 app.use('/', routesPages);
 app.use('/', routesServices);
+
+
+//rotas-------------
+app.patch('/downloadCont/:id', async (req, res) => {
+  try {
+      const id = req.params.id;
+      const game = await Game.findById(id);
+
+      if (!game) {
+          return res.status(404).json({ msg: 'Jogo não encontrado' });
+      }
+
+      const update = { $inc: { download: 1 } };
+
+      // Se `download` não existe, não tente definir diretamente
+      // o MongoDB cuidará disso automaticamente com `$inc`
+      
+      const newUpdate = await Game.findByIdAndUpdate(id, update, { new: true });
+
+      return res.status(200).json({
+          msg: newUpdate.download === 1 ? 'Primeiro download contado com sucesso' : 'Download contado com sucesso',
+          download: newUpdate.download
+      });
+  } catch (err) {
+      console.error('Erro ao contar download:', err);
+      res.status(500).send('Erro ao contar download');
+  }
+})
+
+
+// Rota para o sitemap.xml
+app.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  res.sendFile(SITEMAP_PATH);
+});
+
+
+// Rota para o robots.txt
+app.get('/robots.txt', (req, res) => {
+  res.header('Content-Type', 'text/plain');
+  res.sendFile(ROBOTS_PATH);
+});
+
+
+// Middleware para configurar a política de permissões
+app.use((req, res, next) => {
+  res.setHeader('Set-Cookie', 'cookieName=cookieValue; SameSite=None; Secure');
+  res.setHeader('Permissions-Policy', 'geolocation=(), microphone=(), camera=(), interest-cohort=(), sync-xhr=(), storage=(* "self")');
+  next();
+});
+
+
+// Middleware para lidar com rotas inexistentes
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
 
 
 app.listen(PORT, ()=>{
