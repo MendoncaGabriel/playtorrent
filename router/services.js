444aa65b74b3ec66523fdde4f2dc8a0a512d3372
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-01-06 19:08:02 -0300

    add

--- router/services.js
+++ router/services.js
@@ -4,33 +4,30 @@ const http = require('http');
 const https = require('https');
 const Visitas = require('../model/Visitas.js')
 
-router.post('/constView', async (req, res)=>{
+router.post('/constView', async (req, res) => {
     try {
-        try {
-            // Encontrar o documento correspondente à data atual
-            const hoje = new Date();
-            hoje.setHours(0, 0, 0, 0); // Define a hora para 00:00:00
-            const documentoAtual = await Visitas.findOne({ date: hoje });
-    
-            if (documentoAtual) {
-                // Se já existe um documento para a data atual, incrementar o total de visitas
-                documentoAtual.views += 1;
-                await documentoAtual.save();
-                res.status(200).json({ msg:'Visita registrada com sucesso!, Total: ' + documentoAtual.views});
-            } else {
-                // Se não existe um documento para a data atual, criar um novo documento com total 1
-                const novoDocumento = new Visitas({ views: 1, date: hoje });
-                await novoDocumento.save();
-                res.status(200).json({ msg:'Visita registrada com sucesso!, Total: ' + documentoAtual.views});
-            }
-    
-        } catch (erro) {
-            console.error('Erro ao incrementar o total de visitas:', erro.message);
+        // Verificar se existe um documento com a data atual
+        const hoje = new Date();
+        hoje.setHours(0, 0, 0, 0); // Define a hora para 00:00:00
+
+        const documentoAtual = await Visitas.findOne({ date: hoje });
+
+        if (documentoAtual) {
+            // Se já existe um documento para a data atual, incrementar o total de visitas
+            documentoAtual.views += 1;
+            await documentoAtual.save();
+            res.status(200).json({ msg: 'Visita registrada com sucesso!, Total: ' + documentoAtual.views });
+        } else {
+            // Se não existe um documento para a data atual, criar um novo documento com total 1
+            const novoDocumento = new Visitas({ views: 1, date: hoje });
+            await novoDocumento.save();
+            res.status(200).json({ msg: 'Visita registrada com sucesso!, Total: 1' });
         }
-    } catch (error) {
-        console.log('Erro ao contar view')
+    } catch (erro) {
+        console.error('Erro ao incrementar o total de visitas:', erro.message);
+        res.status(500).json({ error: 'Erro interno do servidor' });
     }
-})
+});
 
 
 //Relatorio de visitas
