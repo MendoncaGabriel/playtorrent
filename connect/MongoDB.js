faed544050480ffd33cb262bb302feadf99d1607
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-01-09 18:07:02 -0300

    add

--- connect/MongoDB.js
+++ connect/MongoDB.js
@@ -6,7 +6,7 @@ const dbuser = process.env.DB_USER;
 const dbpass = process.env.DB_PASS;
 
 function MongoDB(){
-    mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.vuws7v9.mongodb.net/?retryWrites=true&w=majority`)
+    mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${dbuser}:${dbpass}@cluster0.vuws7v9.mongodb.net/?retryWrites=true&w=majority`)
     .then(()=>{
         console.log('Conectado ao MongoDB')
     })
