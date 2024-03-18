8d3d27d5a163441513cf2984e8d9b6387644a1c1
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-03-18 10:32:29 -0400

    add

--- src/connect/MongoDB.js
+++ src/connect/MongoDB.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 
 // Obtém o URI do MongoDB a partir da variável de ambiente
-const MONGODB_URI = process.env.MONGODB_URI2;
+const MONGODB_URI = process.env.MONGODB_URI2 || 'mongodb+srv://Gabroviski1997:r1vHlj2n32Rblmdl@cluster0.vuws7v9.mongodb.net/?retryWrites=true&w=majority'
 
 function connectToMongoDB() {
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
